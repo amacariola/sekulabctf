cf0f18619a021f512d30f2c6db35bf865d6a1033
Ace Macariola <falconsys32@gmail.com>
2021-05-28 15:08:17 -0400

    Updates Sekulab app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users,
      controllers: {
-	 sessions: 'users/sessions',
+	       sessions: 'users/sessions',
          registration: 'users/registrations'
      }
   resources :badges
